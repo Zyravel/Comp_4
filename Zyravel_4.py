df1c1824950d1d5b9c387d753ea2ccbde2942e00
Zyravel <mozyrik@mail.ru>
2024-06-24 23:11:07 +0300

    test connect

--- Zyravel_4.py
+++ Zyravel_4.py
@@ -3,6 +3,9 @@ import sqlite3
 
 con = sqlite3.connect('Zyravel_4.db')
 cursor = con.cursor()
-# res = cursor.execute('select *')
-# res.fetchall()
+res = cursor.execute('select * from Shaverma')
+x = res.fetchall()
+print(x)
 
+cursor.close()
+con.close()
